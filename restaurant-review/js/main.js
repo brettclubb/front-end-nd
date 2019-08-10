7186a529bbab288ad09e69920815f66377dd36d7
brettclubb <brett.clubb@gmail.com>
2019-08-10 13:43:36 -0500

    Adds aria labels. Registers service worker.

--- restaurant-review/js/main.js
+++ restaurant-review/js/main.js
@@ -5,6 +5,22 @@ var newMap
 var markers = []
 
 /**
+ * Register service worker
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
+/**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
